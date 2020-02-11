7fc48355c0e3ecbf40c70426de794ea17d10026c
sunkeyfong <egofang@outlook.com>
2020-02-11 23:44:33 +0800

    A 新增config.page.cache配置，可抓取api数据 （this.apiData） A 新增 config.page.emulate可模拟设备启动 U 优化Page的id, folder为this.html F cli的bug A 引入deepmerge包合并config

--- bin/page.js
+++ bin/page.js
@@ -4,7 +4,6 @@
 module.exports = async( args )=>{
     const $page = new ( require( process.cwd() + '/' + args._.shift() ) )
     var argsArr= []
-
     if( args.argsProvider ){
         console.log('[批量抓取]')
         argsArr = await require( process.cwd() + '/' + args.argsProvider )()
@@ -12,14 +11,14 @@ module.exports = async( args )=>{
 
     for( let i in argsArr ){
         console.group( ( parseInt(i)+1 ) + '/' + argsArr.length )
-        try{ await $page.run.apply( $page, args[i] ) }
+        try{ await $page.run.apply( $page, argsArr[i] ) }
         catch(e){
             console.error( new Error(e) )
             console.log('[ 重启chromiun继续 ]')
             await $page.$browser.close()
             $page.$browser = null
             $page.$page = null
-            await $page.run.apply( $page, args[i] )
+            await $page.run.apply( $page, argsArr[i] )
         }
         console.groupEnd()
     }
--- dist/Page.js
+++ dist/Page.js
@@ -1,12 +1,15 @@
 const fso = require('fs')
 const fs = fso.promises
+const path = require('path')
 const puppeteer = require('puppeteer')
+const deepmerge = require('deepmerge')
+const devices = require('puppeteer/DeviceDescriptors')
 const temme = require('temme').default
 const MongoClient = require('mongodb').MongoClient;
 
 const DEFAULT_PAGE_CONFIG = {
     extract_rule : null, //rule文件path，如果指定将在run后自动extract
-    args : ['key'],  //指定参数的key
+    args : [],  //指定参数的key
     //用于extract
     // mongodb : {
     //     url : 'mongodb://localhost:27017',
@@ -24,6 +27,15 @@ const DEFAULT_PAGE_CONFIG = {
     },
     page : {
         forbid : [],    //屏蔽正则匹配的请求
+        //emulate : 'iPhone X'  指定模拟的设备
+        //捕获请求数据
+        catch : [
+        //     {
+        //         url : 'https://sec-m.ctrip.com/restapi/soa2/12530/json/availableCityListQOC',
+        //         method : 'POST',
+        //         as : 'api_city_list'
+        //     }
+        ],
     },
 
     //@todo catch : [ match => handler( response ) ]    //捕获正则匹配的请求的返回并执行操作
@@ -37,9 +49,9 @@ const DEFAULT_PAGE_CONFIG = {
  * 基本使用方法
  * const detail = new Detail( config )  //将开启新浏览器、新页面
  * await detail.apply( detail , args )
- * console.log( detail.data.html )
+ * console.log( detail.html )
  * await detail.apply( detail , args2 )
- * console.log( detail.data.html )
+ * console.log( detail.html )
  * 
  */
 module.exports = class Page{
@@ -47,8 +59,7 @@ module.exports = class Page{
     /**
      * @override
      */
-    get id(){ return this.args.join('-') }
-    get folder(){ return __dirname + '/' +this.constructor.name  }
+    get html(){ return  __dirname + '/' +this.constructor.name + '/' + (new Date()).getTime() + '.html' }
 
     async action( ...args ){ /** 使页面数据充分展示或补充抓取内容 */ }
     async decorateExtract( data ){ return data }    //extract前修改数据
@@ -59,31 +70,24 @@ module.exports = class Page{
      * @return { Page } page
      */
     constructor( config = {} ){
-        this.config = Object.assign( DEFAULT_PAGE_CONFIG, config )
+        this.config = deepmerge( DEFAULT_PAGE_CONFIG, config )
         this.args = {}
-    }
-
-    get data(){
-        return {
-            html : this.folder + '/' + this.id  + '.html'
-        }
+        this.apiData = {}
     }
 
     /**
      * 抓取页面
      * - 提供过：如果同id文件已存在则跳过
-     * - 更新this.id
      * - 更新this.args
      * - 若无browser将开启新实例
      * - 若无page将开启新实例
      * @param args 继承类要求的参数, 见 this.config.args
      */
-    async run(...args ){
+    async run( ...args ){
         this.config.args.forEach( (n,k) => this.args[n] = args[k] )
         //抓取网页
-        if( fso.existsSync( this.data.html ) ){
-            console.log('[skip crawl]')
-        }else{
+        if( fso.existsSync( this.html ) ) console.log('[skip crawl] 已存在', this.html)
+        else{
             console.log('crawling..')
             await this.__init()
             await this.action.apply( this, args )
@@ -91,80 +95,99 @@ module.exports = class Page{
             await this.__saveContent()
         }
         //提存数据
-        if( !this.config.mongodb ){
-            console.log('[skip extract]','未设置db')
-        }else{
-            this.$collection = this.$collection
-                || await MongoClient.connect( this.config.mongodb.url, this.config.mongodb.options ).then( client => client.db( this.config.mongodb.db ).collection( this.config.mongodb.collection ) )
-            await this.extract( this.data.html, this.$collection )
-        }
+        await this.__extract()
         return this
     }
 
     async __init(){
-        await this.__makePath()
+        await this.__makePath( path.dirname( this.html ) )
         this.$browser = this.$browser || await puppeteer.launch( this.config.browser );
         //设置页面
         if( !this.$page ){
             this.$page = await this.$browser.newPage();
+            if( this.config.page.emulate ) await this.$page.emulate( devices[ this.config.page.emulate ] )
             await this.$page.setBypassCSP(true)  //设置绕过安全策略
+            await this.$page.setRequestInterception(true) //启用请求拦截
             this.__setForbids()
+            this.__setCatches()
         }
     }
+
     __setForbids(){
-        if( this.config.page.fobid.length < 1 ) return
-        console.log('forbid', this.config.page.fobid)
-        this.$page.setRequestInterception(true)
+        if( this.config.page.forbid.length < 1 ) return this.$page.on('request', request => request.continue()  )
         this.$page.on('request', request => {
-            for( let reg of this.config.page.fobid ){
+            for( let reg of this.config.page.forbid ){
                 if( request.url().match( reg ) ){ return request.abort() }
             }
             return request.continue()
         });
     }
-    async __makePath(){
-        if( !fso.existsSync( this.folder ) ){
-            console.error('create folder');
-            await fs.mkdir( this.folder )
+    __setCatches(){
+        if( this.config.page.catch.length < 1 ) return
+        this.$page.on('response', response => {
+            for( let c of this.config.page.catch ){
+                if( response.request().method() == c.method && response.request().url().match( c.url ) ){
+                    let $p = this
+                    try{
+                        response.json().then(json =>  $p.apiData[c.as] = json)
+                    }catch(e){
+                        response.text().then( text => $p.apiData[c.as] = text )
+                    }
+                }
+            }
+        });
+    }
+
+    //迭代创建目录
+    async __makePath( curr ){
+        let dirs = []
+        while( !fso.existsSync(curr) ){
+            dirs.unshift( curr )
+            curr = path.dirname( curr )
         }
+        for( let d of dirs ) await fs.mkdir( d )
     }
 
     /**
      * 保存页面html文件
      */
     async __saveContent(){
-        await fs.writeFile( this.data.html, await this.$page.content() )
-        console.log('saved file '+ this.data.html)
+        await fs.writeFile( this.html, await this.$page.content() )
+        console.log('saved file '+ this.html)
         return this
     }
 
     /**
      * 提取保存后的页面数据到db
      * - 跳过：如果db中已存在同_html的数据则跳过
-     * @param { String } htmlFile html文件path
      */
-    async extract( htmlFile, $clt ){
-        if( !this.config.extract_rule ) return console.log('[skip extract]','rule文件未定义')
+    async __extract(){
+        if( !this.config.mongodb ) return console.log('[skip extract]','未设置db')
+
+        if( !this.$collection ) this.$collection = await MongoClient.connect( this.config.mongodb.url, this.config.mongodb.options ).then( client => client.db( this.config.mongodb.db ).collection( this.config.mongodb.collection ) )
 
         //已存在则跳过
-        if( $clt ){
-            let data = await $clt.findOne({ _html : htmlFile })
-            if( data ){ console.log('[skip extract]'); return data }
-        }
+        if( await this.$collection.findOne({ _html : this.html }) ) return console.log('[skip extract] 已存在')
 
-        //解析并保存
-        this.rule = this.rule || await fs.readFile( this.config.extract_rule ,'UTF-8' )
-        const html = await fs.readFile( htmlFile ,'UTF-8' )
-        data = temme( html , this.rule )
-        data._html = htmlFile
-        data._html_mtime = new Date(fso.statSync( this.data.html ).mtime)
+        let data = Object.assign( {}, this.apiData, this.temme( this.html ) )
+        data._html = this.html
+        data._html_mtime = new Date(fso.statSync( this.html ).mtime)
 
         if( this.decorateExtract ) data = await this.decorateExtract(data)
-        if( $clt ){
-            await $clt.insertOne( data )
-            console.log(`saved mongodb ${this.config.mongodb.db}/${this.config.mongodb.collection}`)
-        }
-        return data
+        await this.$collection.insertOne( data )
+        console.log(`saved mongodb ${this.config.mongodb.db}/${this.config.mongodb.collection}`)
+
+        return this
+    }
+    
+    /**
+     * 使用Page定义的rule解析数据
+     */
+    async temme( htmlFile = null ){
+        if( !this.config.extract_rule ) { console.log('[skip temme]','rule文件未定义'); return null }
+        this.rule = this.rule || await fs.readFile( this.config.extract_rule ,'UTF-8' )
+        const html = await fs.readFile( htmlFile || this.html ,'UTF-8' )
+        return temme( html , this.rule )
     }
 
 }
